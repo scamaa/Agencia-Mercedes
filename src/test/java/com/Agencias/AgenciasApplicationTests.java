0576e98aa7648a8debfa9a178e700f8c46254d91
Santiago <Santiago@SantiPC.tigo.co.cr>
2023-02-09 23:50:44 -0600

    Tarea Semana 4 / Agencia Mercedes

---
+++ src/test/java/com/Agencias/AgenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Agencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
